5f4338678da748877d176d3dd925e30dd1aae526
James Willans <kyoushu@gmail.com>
2015-07-03 12:13:52 +0100

    can now generate filters for liip and avalanche123 imagine bundles

--- src/js/Pufferfish/FilterGenerator/Generator.js
+++ src/js/Pufferfish/FilterGenerator/Generator.js
@@ -28,7 +28,7 @@ define(['jquery'], function($){
     generator.prototype.output = null;
 
     generator.prototype.getFields = function(){
-        return this.form.find('input');
+        return this.form.find('input,select');
     };
 
     generator.prototype.getData = function(){
@@ -48,6 +48,59 @@ define(['jquery'], function($){
     };
 
     generator.prototype.generateOutput = function(){
+        var data = this.getData();
+        if(data.resizer === 'liip'){
+            this.generateLiipOutput();
+        }
+        else{
+            this.generateAvalancheOutput();
+        }
+    };
+
+    generator.prototype.generateLiipOutput = function(){
+
+        var data = this.getData();
+        var output = [];
+
+        output.push('# Generated with Kyoushu/PufferfishFilterGenerator (https://github.com/Kyoushu/PufferfishFilterGenerator)');
+        output.push('# Parameters: ' + JSON.stringify(data));
+        output.push('');
+
+        var widthDiff = parseInt(data.upper_width) - parseInt(data.lower_width);
+
+        var width, height;
+
+        if(data['iterations'] === 1){
+
+            width = parseInt(data.lower_width);
+            height = Math.round(parseInt(data.lower_width) / parseFloat(data.aspect_ratio));
+
+            output.push(data.prefix + '_' + width + ':');
+            output.push('    filters:');
+            output.push('        thumbnail: { size: [' + width + ', ' + height + '], mode: outbound }');
+            output.push('');
+        }
+        else{
+            var increment = widthDiff / (parseInt(data.iterations) - 1);
+
+            for(var i = 0; i < parseInt(data.iterations); i++){
+
+                width = parseInt(data.lower_width) + Math.round(increment * i);
+                height = Math.round(width / parseFloat(data.aspect_ratio));
+
+                output.push(data.prefix + '_' + width + ':');
+                output.push('    filters:');
+                output.push('        thumbnail: { size: [' + width + ', ' + height + '], mode: outbound }');
+                output.push('');
+
+            }
+        }
+
+        this.output.val( output.join("\n") );
+
+    };
+
+    generator.prototype.generateAvalancheOutput = function(){
 
         var data = this.getData();
         var output = [];
