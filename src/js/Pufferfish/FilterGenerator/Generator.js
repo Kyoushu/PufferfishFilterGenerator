f02f5991e0671d364a5c2e20fc3d615468cc6428
James Willans <kyoushu@gmail.com>
2015-08-28 13:47:27 +0100

    Added widen options

--- src/js/Pufferfish/FilterGenerator/Generator.js
+++ src/js/Pufferfish/FilterGenerator/Generator.js
@@ -57,6 +57,24 @@ define(['jquery'], function($){
         }
     };
 
+    generator.prototype.generateLiipFilterOutput = function(prefix, type, width, height){
+
+        var output = [];
+
+        output.push(prefix + '_' + width + ':');
+        output.push('    filters:');
+        output.push('        upscale: { min: [' + width + ', ' + height + '] }');
+        if(type === 'thumbnail'){
+            output.push('        thumbnail: { size: [' + width + ', ' + height + '], mode: outbound, allow_upscale: true }');
+        }
+        if(type === 'widen'){
+            output.push('        relative_resize: { widen: ' + width + ' }');
+        }
+
+        return output.join("\n");
+
+    };
+
     generator.prototype.generateLiipOutput = function(){
 
         var data = this.getData();
@@ -71,35 +89,41 @@ define(['jquery'], function($){
         var width, height;
 
         if(data['iterations'] === 1){
-
             width = parseInt(data.lower_width);
             height = Math.round(parseInt(data.lower_width) / parseFloat(data.aspect_ratio));
-
-            output.push(data.prefix + '_' + width + ':');
-            output.push('    filters:');
-            output.push('        upscale: { min: [' + width + ', ' + height + '] }');
-            output.push('        thumbnail: { size: [' + width + ', ' + height + '], mode: outbound, allow_upscale: true }');
+            output.push( this.generateLiipFilterOutput(data.prefix, data.type, width, height) );
             output.push('');
         }
         else{
             var increment = widthDiff / (parseInt(data.iterations) - 1);
 
             for(var i = 0; i < parseInt(data.iterations); i++){
-
                 width = parseInt(data.lower_width) + Math.round(increment * i);
                 height = Math.round(width / parseFloat(data.aspect_ratio));
-
-                output.push(data.prefix + '_' + width + ':');
-                output.push('    filters:');
-                output.push('        upscale: { min: [' + width + ', ' + height + '] }');
-                output.push('        thumbnail: { size: [' + width + ', ' + height + '], mode: outbound, allow_upscale: true }');
+                output.push( this.generateLiipFilterOutput(data.prefix, data.type, width, height) );
                 output.push('');
-
             }
         }
 
         this.output.val( output.join("\n") );
 
+    };
+
+    generator.prototype.generateAvalanceFilterOutput = function(prefix, type, width, height){
+        var output = [];
+
+        output.push(prefix + '_' + width + ':');
+
+        if(type === 'widen'){
+            output.push('    type: relative_resize');
+            output.push('    options: { widen: ' + width + ' }');
+        }
+        else if(type === 'thumbnail'){
+            output.push('    type: thumbnail');
+            output.push('    options: { size: [' + width + ', ' + height + '], mode: outbound }');
+        }
+
+        return output.join("\n");
     };
 
     generator.prototype.generateAvalancheOutput = function(){
@@ -116,28 +140,19 @@ define(['jquery'], function($){
         var width, height;
 
         if(data['iterations'] === 1){
-
             width = parseInt(data.lower_width);
             height = Math.round(parseInt(data.lower_width) / parseFloat(data.aspect_ratio));
-
-            output.push(data.prefix + '_' + width + ':');
-            output.push('    type: thumbnail');
-            output.push('    options: { size: [' + width + ', ' + height + '], mode: outbound }');
+            output.push(this.generateAvalanceFilterOutput(data.prefix, data.type, width, height));
             output.push('');
         }
         else{
             var increment = widthDiff / (parseInt(data.iterations) - 1);
 
             for(var i = 0; i < parseInt(data.iterations); i++){
-
                 width = parseInt(data.lower_width) + Math.round(increment * i);
                 height = Math.round(width / parseFloat(data.aspect_ratio));
-
-                output.push(data.prefix + '_' + width + ':');
-                output.push('    type: thumbnail');
-                output.push('    options: { size: [' + width + ', ' + height + '], mode: outbound }');
+                output.push(this.generateAvalanceFilterOutput(data.prefix, data.type, width, height));
                 output.push('');
-
             }
         }
 
